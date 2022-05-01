d5a8878adb9e24ab46857e4ffbc6f1d8be472812
Shamistan Aliyev~ <shamistanaliyev237@gmail.com>
2022-05-01 23:45:36 +0400

    .

--- Controller/controller.mjs
+++ Controller/controller.mjs
@@ -8,109 +8,84 @@ export default class Controller {
     this.render();
   }
 
-  render() {
-    let input_currency = document.querySelectorAll(".money-input");
+  async getDataFromApi(from, to) {
+    let url = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
 
-    let left_currency_value = document.querySelector(
+    const res = await fetch(url);
+    const data = await res.json();
+
+    this.model.resultOfData = data.result;
+  }
+
+  valueConvertText() {
+    const rateFrom = document.getElementById("rateFrom");
+    rateFrom.innerText = `1 ${
+      this.view.left_currency_value
+    } = ${this.model.resultOfData.toFixed(6)} ${
+      this.view.right_currency_value
+    }`;
+
+    const rateIn = document.getElementById("rateIn");
+    rateIn.innerText = `1 ${this.view.right_currency_value} =  ${(
+      1 / this.model.resultOfData
+    ).toFixed(6)} ${this.view.left_currency_value}`;
+  }
+
+  render() {
+    this.input_currency = document.querySelectorAll(".money-input");
+
+    this.view.left_currency_value = document.querySelector(
       "input[name='radio1']:checked"
     ).value;
 
-    let right_currency_value = document.querySelector(
+    this.view.right_currency_value = document.querySelector(
       "input[name='radio2']:checked"
     ).value;
 
-    let resultOfData = null;
-
-    async function getDataFromApi(from, to) {
-      let url = `https://api.exchangerate.host/convert?from=${from}&to=${to}`;
-
-      const res = await fetch(url);
-      const data = await res.json();
-
-      resultOfData = data.result;
-    }
-
-    getDataFromApi(left_currency_value, right_currency_value);
-
-    function convertValue(leftCurrency, rightCurrency) {
-      rightCurrency.value = leftCurrency.value * resultOfData;
-
-      const rateFrom = document.getElementById("rateFrom");
-      rateFrom.innerText = `1 ${left_currency_value} = ${resultOfData.toFixed()} ${right_currency_value}`;
-
-      const rateIn = document.getElementById("rateIn");
-      rateIn.innerText = `1 ${right_currency_value} =  ${(
-        1 / resultOfData
-      ).toFixed(6)} ${left_currency_value}`;
-    }
+    this.getDataFromApi(
+      this.view.left_currency_value,
+      this.view.right_currency_value
+    );
 
     this.model.radio1.forEach((el) => {
       el.addEventListener("click", async () => {
-        left_currency_value = document.querySelector(
+        this.view.left_currency_value = document.querySelector(
           "input[name='radio1']:checked"
         ).value;
 
-        await getDataFromApi(left_currency_value, right_currency_value);
-        convertValue(input_currency[0], input_currency[1]);
+        await this.getDataFromApi(
+          this.view.left_currency_value,
+          this.view.right_currency_value
+        );
+        this.model.convertValue(this.input_currency[0], this.input_currency[1]);
 
-        const rateFrom = document.getElementById("rateFrom");
-        rateFrom.innerText = `1 ${left_currency_value} = ${resultOfData.toFixed(
-          6
-        )} ${right_currency_value}`;
-
-        const rateIn = document.getElementById("rateIn");
-        rateIn.innerText = `1 ${right_currency_value} =  ${(
-          1 / resultOfData
-        ).toFixed(6)} ${left_currency_value}`;
+        this.valueConvertText();
       });
     });
 
     this.model.radio2.forEach((el) => {
       el.addEventListener("click", async () => {
-        right_currency_value = document.querySelector(
+        this.view.right_currency_value = document.querySelector(
           "input[name='radio2']:checked"
         ).value;
-        await getDataFromApi(left_currency_value, right_currency_value);
-        convertValue(input_currency[0], input_currency[1]);
+        await this.getDataFromApi(
+          this.view.left_currency_value,
+          this.view.right_currency_value
+        );
+        this.model.convertValue(this.input_currency[0], this.input_currency[1]);
 
-        const rateFrom = document.getElementById("rateFrom");
-        rateFrom.innerText = `1 ${left_currency_value} = ${resultOfData.toFixed(
-          6
-        )} ${right_currency_value}`;
-
-        const rateIn = document.getElementById("rateIn");
-        rateIn.innerText = `1 ${right_currency_value} =  ${(
-          1 / resultOfData
-        ).toFixed(6)} ${left_currency_value}`;
+        this.valueConvertText();
       });
     });
 
-    input_currency[0].addEventListener("keyup", () => {
-      convertValue(input_currency[0], input_currency[1]);
-
-      const rateFrom = document.getElementById("rateFrom");
-      rateFrom.innerText = `1 ${left_currency_value} = ${resultOfData.toFixed(
-        6
-      )} ${right_currency_value}`;
-
-      const rateIn = document.getElementById("rateIn");
-      rateIn.innerText = `1 ${right_currency_value} =  ${(
-        1 / resultOfData
-      ).toFixed(6)} ${left_currency_value}`;
+    this.input_currency[0].addEventListener("keyup", () => {
+      this.model.convertValue(this.input_currency[0], this.input_currency[1]);
+      this.valueConvertText();
     });
 
-    input_currency[1].addEventListener("keyup", () => {
-      convertValue(input_currency[1], input_currency[0]);
-
-      const rateFrom = document.getElementById("rateFrom");
-      rateFrom.innerText = `1 ${left_currency_value} = ${resultOfData.toFixed(
-        6
-      )} ${right_currency_value}`;
-
-      const rateIn = document.getElementById("rateIn");
-      rateIn.innerText = `1 ${right_currency_value} =  ${(
-        1 / resultOfData
-      ).toFixed(6)} ${left_currency_value}`;
+    this.input_currency[1].addEventListener("keyup", () => {
+      this.model.convertValue(this.input_currency[1], this.input_currency[0]);
+      this.valueConvertText();
     });
   }
 }
--- View/view.mjs
+++ View/view.mjs
@@ -1,3 +1,11 @@
 export default class View {
-  constructor() {}
+  constructor() {
+    this.left_currency_value = document.querySelector(
+      "input[name='radio1']:checked"
+    );
+
+    this.right_currency_value = document.querySelector(
+      "input[name='radio2']:checked"
+    );
+  }
 }
